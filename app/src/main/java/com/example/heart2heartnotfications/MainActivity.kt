7e09565a910f858327891bb8eb99560233079e81
jonas <joni0014@stud.ek.dk>
2026-04-07 09:44:15 +0200

    Start

--- app/src/main/java/com/example/heart2heartnotfications/MainActivity.kt
+++ app/src/main/java/com/example/heart2heartnotfications/MainActivity.kt
@@ -12,20 +12,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.heart2heartnotfications.ui.theme.Heart2heartNotficationsTheme
+import com.example.heart2heartnotfications.view.settingsUI
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Heart2heartNotficationsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            settingsUI()
         }
     }
 }
---
+++ app/src/main/java/com/example/heart2heartnotfications/view/settingsUI.kt
@@ -0,0 +1,14 @@
+package com.example.heart2heartnotfications.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun settingsUI() {
+    Column() {
+        Text("Hej")
+    }
+}
