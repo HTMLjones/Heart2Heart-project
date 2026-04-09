390eb0f07aecbb3fdb4bfb76bcb700e8992b94c3
jonas <joni0014@stud.ek.dk>
2026-04-09 12:18:40 +0200

    Opsætning af noti

--- app/src/main/java/com/example/heart2heartnotfications/MainActivity.kt
+++ app/src/main/java/com/example/heart2heartnotfications/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.heart2heartnotfications.ui.theme.Heart2heartNotficationsTheme
 import com.example.heart2heartnotfications.view.premiumNotification
 import com.example.heart2heartnotfications.view.profileSection
--- app/src/main/java/com/example/heart2heartnotfications/view/settingsUI.kt
+++ app/src/main/java/com/example/heart2heartnotfications/view/settingsUI.kt
@@ -1,5 +1,6 @@
 package com.example.heart2heartnotfications.view
 
+import android.R.attr.text
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
@@ -16,7 +18,13 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -92,7 +100,10 @@ fun ProfileRow(label: String, value: String) {
         Text(text = label, fontWeight = FontWeight.Bold, fontSize = 15.sp)
         Text(text = value, fontSize = 15.sp)
     }
+
 }
+
+
 
 @Preview
 @Composable
@@ -103,7 +114,9 @@ fun premiumNotification() {
     //Grå farve
     val gray = Color(0xFF7E7E7E)
 
-    Card(modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp, vertical = 350.dp), shape = RoundedCornerShape(20.dp), border = BorderStroke(1.5.dp, pink), colors = CardDefaults.cardColors(containerColor = Color.White)) {
+
+
+    Card(modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp, vertical = 8.dp), shape = RoundedCornerShape(20.dp), border = BorderStroke(1.5.dp, pink), colors = CardDefaults.cardColors(containerColor = Color.White)) {
         Column(modifier = Modifier.padding(16.dp)) {
             Row(modifier = Modifier.fillMaxWidth(),Arrangement.SpaceBetween,Alignment.CenterVertically) {
                 Text(text= "Change Notification", color=gray, fontSize = 14.sp)
@@ -112,24 +125,38 @@ fun premiumNotification() {
 
             Spacer(Modifier.height(12.dp))
 
-            Column(modifier = Modifier.fillMaxWidth(),Arrangement.SpaceBetween,Alignment.Start) {
-                Message("Level 1", "2")
-                Message("Level 2", "2")
-                Message("Level 3", "2")
+            Column(modifier = Modifier.fillMaxWidth(), verticalArrangement = Arrangement.SpaceBetween, horizontalAlignment = Alignment.Start) {
+                Message("Level 1", standardText = "2")
+                Message("Level 2", standardText = "2")
+                Message("Level 3", standardText = "2")
             }
         }
     }
 }
 
 @Composable
-fun Message(label: String, value: String) {
+fun Message(label: String, standardText: String) {
+    var newText by remember { mutableStateOf(standardText) }
     Column(
         modifier = Modifier
+            .fillMaxWidth()
             .padding(vertical = 6.dp),
-         Arrangement.SpaceBetween
+            Arrangement.SpaceBetween,
+            Alignment.Start
     ) {
         Text(text = label, fontWeight = FontWeight.Bold, fontSize = 15.sp)
-        Text(text = value, fontSize = 15.sp)
+        TextField(
+            value = newText,
+            onValueChange = { newText = it },
+            singleLine = true,
+            modifier = Modifier.width(300.dp),
+            colors = TextFieldDefaults.colors(
+                unfocusedContainerColor = Color(0xFFFFE8F4),
+                focusedContainerColor = Color.Transparent
+            ),
+            shape = RoundedCornerShape(100.dp),
+
+        )
     }
 }
 
